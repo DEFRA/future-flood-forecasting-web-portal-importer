90663c0004f62946a5eb6ae3010138197666551d
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2019-09-13 15:09:38 +0100

    Implemented first real unit test

--- ImportTimeSeriesDisplayGroups/index.test.js
+++ ImportTimeSeriesDisplayGroups/index.test.js
@@ -1,7 +1,60 @@
-const context = require('../testing/mocks/defaultContext')
+const axios = require('axios')
+const queueFunction = require('./index')
+const taskRunCompleteMessages = require('../testing/messages/task-run-complete/messages')
+const { pool, pooledConnect, sql } = require('../Shared/connection-pool')
 
-// Temporary dummy test to check if Jest is set up correctly.
-test('Always pass', async () => {
-  context.log('This test always passes')
-  expect(true).toBe(true)
+let request
+let context
+jest.mock('axios')
+
+beforeAll(() => {
+  // Ensure the connection pool is ready
+  return pooledConnect
+})
+
+beforeAll(() => {
+  request = new sql.Request(pool)
+  return request
+})
+
+beforeAll(() => {
+  return request.batch(`
+    insert into
+      ${process.env['FFFS_WEB_PORTAL_STAGING_DB_STAGING_SCHEMA']}.location_lookup (workflow_id, plot_id, location_ids)
+    values
+      ('Test_Workflow', 'Test Plot', 'Test Location')
+  `)
+})
+
+beforeEach(() => {
+  // As mocks are reset and restored between each test (through configuration in package.json), the Jest mock
+  // function implementation for the function context needs creating for each test.
+  context = require('../testing/mocks/defaultContext')
+  return request.batch(`truncate table ${process.env['FFFS_WEB_PORTAL_STAGING_DB_STAGING_SCHEMA']}.timeseries`)
+})
+
+afterAll(() => {
+  return pool.close()
+})
+
+describe('Import timeseries display groups', () => {
+  it('should import data for an approved forecast', async () => {
+    const mockDataValue = 'Timeseries display groups data'
+    const mockResponseData = {
+      data: mockDataValue
+    }
+
+    axios.get.mockResolvedValue(mockResponseData)
+
+    await queueFunction(context, JSON.stringify(taskRunCompleteMessages['approvedForecast']))
+    const result = await request.query(`
+      select
+        -- Remove double quotes surrounding the value returned from the database.
+        substring(fews_data, 2, len(fews_data) -2) as fews_data
+      from
+        ${process.env['FFFS_WEB_PORTAL_STAGING_DB_STAGING_SCHEMA']}.timeseries
+    `)
+
+    expect(result.recordset[0].fews_data).toBe(mockDataValue)
+  })
 })
