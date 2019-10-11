25c8444cee019bf8db32c718edd0a2c41cf75f94
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2019-10-11 11:34:51 +0100

    Switched to common build command

--- build/src/main/resources/scripts/build.sh
+++ build/src/main/resources/scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+rm -f ImportTimeSeriesDisplayGroups/function.json
+rm -f RefreshLocationLookupData/function.json
 mvn clean -f build/pom.xml process-resources
 cp build/target/host.json.template host.json
-cp build/src/main/resources/functions/ImportTimeSeriesDisplayGroups/$BUILD_TYPE/function.json ImportTimeSeriesDisplayGroups/
-cp build/src/main/resources/functions/RefreshLocationLookupData/$BUILD_TYPE/function.json RefreshLocationLookupData/
+cp build/src/main/resources/functions/ImportTimeSeriesDisplayGroups/$FFFS_WEB_PORTAL_BUILD_TYPE/function.json ImportTimeSeriesDisplayGroups/
+cp build/src/main/resources/functions/RefreshLocationLookupData/$FFFS_WEB_PORTAL_BUILD_TYPE/function.json RefreshLocationLookupData/
