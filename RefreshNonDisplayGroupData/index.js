931779be78f999c429842c0bc6eedf7e99ce0bc7
Adam-W1 <adam.wallace@environment-agency.gov.uk>
2019-12-11 12:27:37 +0000

    testing fixes

--- RefreshNonDisplayGroupData/index.js
+++ RefreshNonDisplayGroupData/index.js
@@ -4,19 +4,20 @@ const neatCsv = require('neat-csv')
 const sql = require('mssql')
 
 module.exports = async function (context, message) {
+  async function refresh (transactionData, context) {
+    await refreshNonDisplayGroupData(transactionData.preparedStatement, transactionData.transaction, context)
+  }
   // Refresh the data in the fluvial_non_display_group_workflow table within a transaction with a serializable isolation
   // level so that refresh is prevented if the fluvial_non_display_group_workflow table is in use. If the fluvial_non_display_group_workflow
   // table is in use and fluvial_non_display_group_workflow table lock acquisition fails, the function invocation will fail.
   // In most cases function invocation will be retried automatically and should succeed.  In rare
   // cases where successive retries fail, the message that triggers the function invocation will be
   // placed on a dead letter queue.  In this case, manual intervention will be required.
-  await doInTransaction(refreshNonDisplayGroupData, context, 'The FLUVIAL_NON_DISPLAY_GROUP_WORKFLOW refresh has failed with the following error:', sql.ISOLATION_LEVEL.SERIALIZABLE)
+  await doInTransaction(refresh, context, 'The FLUVIAL_NON_DISPLAY_GROUP_WORKFLOW refresh has failed with the following error:', sql.ISOLATION_LEVEL.SERIALIZABLE)
   // context.done() not requried as the async function returns the desired result, there is no output binding to be activated.
 }
 
-async function refreshNonDisplayGroupData (transactionData, context) {
-  const preparedStatement = transactionData.preparedStatement
-  const transaction = transactionData.transaction
+async function refreshNonDisplayGroupData (preparedStatement, transaction, context) {
   context.log.info('running')
   try {
     const response = await fetch(`${process.env['FLUVIAL_NON_DISPLAY_GROUP_WORKFLOW_URL']}`)
