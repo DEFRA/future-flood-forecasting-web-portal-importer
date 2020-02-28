4811bb82a83acc850acc194ebcad323eab364209
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2020-02-28 19:16:56 +0000

    Remove incorrect comment

--- ImportTimeseriesRouter/helpers/is-task-run-imported.js
+++ ImportTimeseriesRouter/helpers/is-task-run-imported.js
@@ -3,9 +3,6 @@ const sql = require('mssql')
 module.exports = async function isTaskRunImported (context, preparedStatement, taskId) {
   await preparedStatement.input('taskId', sql.NVarChar)
 
-  // Run the query to retrieve non display group data in a full transaction with a table lock held
-  // for the duration of the transaction to guard against a non display group data refresh during
-  // data retrieval.
   await preparedStatement.prepare(`
     select
       count(task_id) as task_id_count
