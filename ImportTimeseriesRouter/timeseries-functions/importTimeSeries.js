2e3eeb8a6be37446518c40c691b0370f53c969fd
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2020-03-02 16:50:48 +0000

    Add task ID to non-display group calls

--- ImportTimeseriesRouter/timeseries-functions/importTimeSeries.js
+++ ImportTimeseriesRouter/timeseries-functions/importTimeSeries.js
@@ -23,12 +23,13 @@ async function getTimeseriesInternal (nonDisplayGroupData, routeData) {
   // the REST interface of the core forecasting engine requires fractional seconds to be excluded from dates.
   const fewsStartTime = `&startTime=${routeData.startTime.substring(0, 19)}Z`
   const fewsEndTime = `&endTime=${routeData.endTime.substring(0, 19)}Z`
+  const fewsTaskRunIds = `&taskRunIds=${routeData.taskId}`
 
   const timeseriesNonDisplayGroupsData = []
 
   for (const value of nonDisplayGroupData) {
     const filterId = `&filterId=${value}`
-    const fewsParameters = `${filterId}${fewsStartTime}${fewsEndTime}`
+    const fewsParameters = `${filterId}${fewsTaskRunIds}${fewsStartTime}${fewsEndTime}`
 
     // Get the timeseries display groups for the configured plot, locations and date range.
     const fewsPiEndpoint = encodeURI(`${process.env['FEWS_PI_API']}/FewsWebServices/rest/fewspiservice/v1/timeseries?useDisplayUnits=false&showThresholds=true&showProducts=false&omitMissing=true&onlyHeaders=false&showEnsembleMemberIds=false&documentVersion=1.26&documentFormat=PI_JSON&forecastCount=1${fewsParameters}`)
