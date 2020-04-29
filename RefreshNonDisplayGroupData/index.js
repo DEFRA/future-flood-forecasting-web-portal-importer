0a103786b140ccfed58477c8ca577ca3ca374c1a
Adam-W1 <adam.wallace@environment-agency.gov.uk>
2020-04-29 16:49:15 +0100

    NDG function exception testing

--- RefreshNonDisplayGroupData/index.js
+++ RefreshNonDisplayGroupData/index.js
@@ -1,13 +1,14 @@
 const { doInTransaction, executePreparedStatementInTransaction } = require('../Shared/transaction-helper')
 const { isBoolean } = require('../Shared/utils')
-const createCSVStagingException = require('../Shared/failed-csv-load-handler/create-csv-staging-exception')
+const loadExceptions = require('../Shared/failed-csv-load-handler/load-csv-exceptions')
 const fetch = require('node-fetch')
 const neatCsv = require('neat-csv')
 const sql = require('mssql')
 
 module.exports = async function (context, message) {
+  let failedRows
   async function refresh (transaction, context) {
-    await executePreparedStatementInTransaction(refreshNonDisplayGroupData, context, transaction)
+    failedRows = await executePreparedStatementInTransaction(refreshNonDisplayGroupData, context, transaction)
   }
   // Refresh the data in the non_display_group_workflow table within a transaction with a serializable isolation
   // level so that refresh is prevented if the non_display_group_workflow table is in use. If the non_display_group_workflow
@@ -16,12 +17,21 @@ module.exports = async function (context, message) {
   // cases where successive retries fail, the message that triggers the function invocation will be
   // placed on a dead letter queue.  In this case, manual intervention will be required.
   await doInTransaction(refresh, context, 'The non_display_group_workflow refresh has failed with the following error:', sql.ISOLATION_LEVEL.SERIALIZABLE)
+
+  // Transaction 2
+  if (failedRows.length > 0) {
+    context.log.error(`The non display group csv loader has ${failedRows.length} failed row inserts.`)
+    await doInTransaction(loadExceptions, context, 'The ignored workflow exception load has failed with the following error:', sql.ISOLATION_LEVEL.SERIALIZABLE, 'triton coastal locations', failedRows)
+  } else {
+    context.log.info(`There were no csv exceptions during load.`)
+  }
   // context.done() not requried as the async function returns the desired result, there is no output binding to be activated.
+
 }
 
 async function refreshNonDisplayGroupData (context, preparedStatement) {
-  context.log.info('running')
   const transaction = preparedStatement.parent
+  const failedRows = []
   try {
     const response = await fetch(`${process.env['NON_DISPLAY_GROUP_WORKFLOW_URL']}`)
     const rows = await neatCsv(response.body)
@@ -31,7 +41,6 @@ async function refreshNonDisplayGroupData (context, preparedStatement) {
       const request = new sql.Request(preparedStatement.parent)
       await request.batch(`delete from ${process.env['FFFS_WEB_PORTAL_STAGING_DB_STAGING_SCHEMA']}.non_display_group_workflow`)
 
-      const failedRows = []
       // set the input values up - limit to type
       await preparedStatement.input('WORKFLOW_ID', sql.NVarChar)
       await preparedStatement.input('FILTER_ID', sql.NVarChar)
@@ -82,18 +91,6 @@ async function refreshNonDisplayGroupData (context, preparedStatement) {
       }
       // Future requests will fail until the prepared statement is unprepared.
       await preparedStatement.unprepare()
-
-      for (let i = 0; i < failedRows.length; i++) {
-        await executePreparedStatementInTransaction(
-          createCSVStagingException, // function
-          context, // context
-          transaction, // transaction
-          `Non display group data`, // args - csv file
-          failedRows[i].rowData, // args - row data
-          failedRows[i].errorMessage // args - error description
-        )
-      }
-      context.log.error(`The non display group csv loader has ${failedRows.length} failed row inserts.`)
     } else {
       // If the csv is empty then the file is essentially ignored
       context.log.warn('No records detected - Aborting non_display_group_workflow refresh')
@@ -115,6 +112,8 @@ async function refreshNonDisplayGroupData (context, preparedStatement) {
       await transaction.rollback()
       context.log.info('Transaction rolled back.')
     }
+
+    return failedRows
   } catch (err) {
     context.log.error(`Refresh non display group workflow data failed: ${err}`)
     throw err
