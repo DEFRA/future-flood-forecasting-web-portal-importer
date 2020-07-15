215f1e68ac2d1ecd1c7148abdf57285610d3234b
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2020-07-15 15:24:33 +0100

    Use ProcessFewsEventCode function

--- build/src/main/resources/scripts/build.sh
+++ build/src/main/resources/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy the configuration file for each function into place based on whether a queue or topic build is being performed. 
-rm -f ImportTimeseriesRouter/function.json
+rm -f ProcessFewsEventCode/function.json
 rm -f RefreshFluvialDisplayGroupData/function.json
 rm -f RefreshCoastalDisplayGroupData/function.json
 rm -f RefreshNonDisplayGroupData/function.json
@@ -13,11 +13,7 @@ rm -f RefreshIgnoredWorkflowData/function.json
 rm -f DeleteExpiredTimeseries/function.json
 mvn clean -f build/pom.xml process-resources
 cp build/target/host.json.template host.json
-if [[ "${IMPORT_TIMESERIES_OUTPUT_BINDING_REQUIRED}" == "true" ]]; then
-  cp build/src/main/resources/functions/ImportTimeseriesRouter/OutputBinding/$FFFS_WEB_PORTAL_BUILD_TYPE/function.json ImportTimeseriesRouter/
-else
-  cp build/src/main/resources/functions/ImportTimeseriesRouter/NoOutputBinding/$FFFS_WEB_PORTAL_BUILD_TYPE/function.json ImportTimeseriesRouter/
-fi
+cp build/src/main/resources/functions/ProcessFewsEventCode/$FFFS_WEB_PORTAL_BUILD_TYPE/function.json ProcessFewsEventCode/
 cp build/src/main/resources/functions/RefreshFluvialDisplayGroupData/$FFFS_WEB_PORTAL_BUILD_TYPE/function.json RefreshFluvialDisplayGroupData/
 cp build/src/main/resources/functions/RefreshCoastalDisplayGroupData/$FFFS_WEB_PORTAL_BUILD_TYPE/function.json RefreshCoastalDisplayGroupData/
 cp build/src/main/resources/functions/RefreshNonDisplayGroupData/$FFFS_WEB_PORTAL_BUILD_TYPE/function.json RefreshNonDisplayGroupData/
