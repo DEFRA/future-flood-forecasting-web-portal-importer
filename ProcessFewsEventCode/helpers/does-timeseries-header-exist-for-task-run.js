43b618be6c9661664f57386e81753366de4c9b2f
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2020-08-07 15:48:04 +0100

    Ignore messages for procsssed task runs

---
+++ ProcessFewsEventCode/helpers/does-timeseries-header-exist-for-task-run.js
@@ -0,0 +1,22 @@
+const sql = require('mssql')
+
+module.exports = async function (context, preparedStatement, taskRunData) {
+  await preparedStatement.input('taskRunId', sql.NVarChar)
+
+  await preparedStatement.prepare(`
+      select
+        id
+      from
+        fff_staging.timeseries_header
+      where
+        task_run_id = @taskRunId     
+    `)
+
+  const parameters = {
+    taskRunId: taskRunData.taskRunId
+  }
+
+  const result = await preparedStatement.execute(parameters)
+
+  return !!(result.recordset && result.recordset[0])
+}
