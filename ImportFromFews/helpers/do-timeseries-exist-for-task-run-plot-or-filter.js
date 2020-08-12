88ad5a8f5892512b51aa37f60957169537ae7e3c
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2020-08-12 16:47:02 +0100

    Facilitate replay for plots when a subset of locations has been loaded

---
+++ ImportFromFews/helpers/do-timeseries-exist-for-task-run-plot-or-filter.js
@@ -0,0 +1,33 @@
+const { executePreparedStatementInTransaction } = require('../../Shared/transaction-helper')
+const sql = require('mssql')
+
+module.exports = async function (context, taskRunData) {
+  return Promise.resolve(await executePreparedStatementInTransaction(doTimeseriesExistForTaskRunPlotOrFilter, context, taskRunData.transaction, taskRunData))
+}
+
+async function doTimeseriesExistForTaskRunPlotOrFilter (context, preparedStatement, taskRunData) {
+  const fewsParameters = taskRunData.plotId ? `&plotId=${taskRunData.plotId}%` : `&filterId=${taskRunData.filterId}%`
+  await preparedStatement.input('taskRunId', sql.NVarChar)
+  await preparedStatement.input('fewsParameters', sql.NVarChar)
+
+  await preparedStatement.prepare(`
+    select
+      t.id
+    from
+      fff_staging.timeseries_header th,
+      fff_staging.timeseries t
+     where
+      th.task_run_id = @taskRunId and
+      th.id = t.timeseries_header_id and
+      t.fews_parameters like @fewsParameters     
+    `)
+
+  const parameters = {
+    taskRunId: taskRunData.taskRunId,
+    fewsParameters: fewsParameters
+  }
+
+  const result = await preparedStatement.execute(parameters)
+
+  return !!(result && result.recordset && result.recordset[0])
+}
