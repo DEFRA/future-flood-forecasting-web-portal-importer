ab2832fce49adc260c42625edda8fa4f2bb545c7
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2020-09-11 17:09:21 +0100

    Added clone operation

--- testing/staging-database/bootstrap.sh
+++ testing/staging-database/bootstrap.sh
@@ -2,7 +2,8 @@
 
 if  ! `nc -z $SQLTESTDB_HOST $SQLTESTDB_PORT`; then
   cd testing/staging-database
-  rm -rf future-flood-forecasting-web-portal-staging 
+  rm -rf future-flood-forecasting-web-portal-staging
+  git clone https://github.com/DEFRA/future-flood-forecasting-web-portal-staging.git
   cd future-flood-forecasting-web-portal-staging
   ./local-bootstrap.sh
 fi
