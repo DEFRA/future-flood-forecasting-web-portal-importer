6a40bb14e8bd311060511e31b147ba7b74c537ac
Adam-W1 <adam.wallace@environment-agency.gov.uk>
2020-10-15 11:14:25 +0100

    allow null values in certain mvt columns

--- RefreshMVTData/index.js
+++ RefreshMVTData/index.js
@@ -9,17 +9,17 @@ module.exports = async function (context, message) {
     partialTableUpdate: { flag: false },
     // Column information and corresponding csv information
     functionSpecificData: [
-      { tableColumnName: 'CENTRE', tableColumnType: 'NVarChar', expectedCSVKey: 'Centre' },
-      { tableColumnName: 'CRITICAL_CONDITION_ID', tableColumnType: 'NVarChar', expectedCSVKey: 'criticalConditionID' },
+      { tableColumnName: 'CENTRE', tableColumnType: 'NVarChar', expectedCSVKey: 'Centre', nullValueOverride: true },
+      { tableColumnName: 'CRITICAL_CONDITION_ID', tableColumnType: 'NVarChar', expectedCSVKey: 'criticalConditionID', nullValueOverride: true },
       { tableColumnName: 'INPUT_LOCATION_ID', tableColumnType: 'NVarChar', expectedCSVKey: 'inputLocationID' },
       { tableColumnName: 'OUTPUT_LOCATION_ID', tableColumnType: 'NVarChar', expectedCSVKey: 'outputLocationID' },
-      { tableColumnName: 'TARGET_AREA_CODE', tableColumnType: 'NVarChar', expectedCSVKey: 'TargetAreaCode' },
+      { tableColumnName: 'TARGET_AREA_CODE', tableColumnType: 'NVarChar', expectedCSVKey: 'TargetAreaCode', nullValueOverride: true },
       { tableColumnName: 'INPUT_PARAMETER_ID', tableColumnType: 'NVarChar', expectedCSVKey: 'inputParameterID' },
-      { tableColumnName: 'LOWER_BOUND', tableColumnType: 'Decimal', expectedCSVKey: 'lowerBound', precision: 5, scale: 2 },
-      { tableColumnName: 'UPPER_BOUND', tableColumnType: 'Decimal', expectedCSVKey: 'upperBound', precision: 5, scale: 2 },
-      { tableColumnName: 'LOWER_BOUND_INCLUSIVE', tableColumnType: 'Bit', expectedCSVKey: 'lowerBoundInclusive', preprocessor: parseBooleanString },
-      { tableColumnName: 'UPPER_BOUND_INCLUSIVE', tableColumnType: 'Bit', expectedCSVKey: 'upperBoundInclusive', preprocessor: parseBooleanString },
-      { tableColumnName: 'PRIORITY', tableColumnType: 'Int', expectedCSVKey: 'Priority' }
+      { tableColumnName: 'LOWER_BOUND', tableColumnType: 'Decimal', expectedCSVKey: 'lowerBound', precision: 5, scale: 2, nullValueOverride: true, preprocessor: returnNullForNaN },
+      { tableColumnName: 'UPPER_BOUND', tableColumnType: 'Decimal', expectedCSVKey: 'upperBound', precision: 5, scale: 2, nullValueOverride: true, preprocessor: returnNullForNaN },
+      { tableColumnName: 'LOWER_BOUND_INCLUSIVE', tableColumnType: 'Bit', expectedCSVKey: 'lowerBoundInclusive', preprocessor: parseBooleanString, nullValueOverride: true },
+      { tableColumnName: 'UPPER_BOUND_INCLUSIVE', tableColumnType: 'Bit', expectedCSVKey: 'upperBoundInclusive', preprocessor: parseBooleanString, nullValueOverride: true },
+      { tableColumnName: 'PRIORITY', tableColumnType: 'Int', expectedCSVKey: 'Priority', nullValueOverride: true }
     ],
     type: 'mvt'
   }
@@ -28,4 +28,12 @@ module.exports = async function (context, message) {
 
 function parseBooleanString (booleanString) {
   return JSON.parse(booleanString.toLowerCase())
+}
+
+function returnNullForNaN (value) {
+  if (isNaN(value) || value === '') {
+    return null
+  } else {
+    return value
+  }
 }
