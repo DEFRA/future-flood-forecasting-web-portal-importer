5fb06bbc436d2cef9fded587cc4145e383d8b76e
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2020-11-06 13:24:43 +0000

    Allow replay of core engine messages containing invalid JSON
    Part of https://eaflood.atlassian.net/browse/IWP-638

--- ReplayProcessFewsEventCode/index.js
+++ ReplayProcessFewsEventCode/index.js
@@ -1,4 +1,17 @@
 module.exports = async function (context, message) {
   context.log(`Replaying ${message}`)
-  context.bindings.processFewsEventCode = message
+  // If JSON parsing fails when a message is sent to the fews-eventcode queue the message
+  // will remain on the dead letter queue. It seems that some core forecasting engine messages
+  // can contain invalid JSON, causing this scanario. To workaround this temporarily, try and parse
+  // the message as JSON before replay is attempted. If parsing fails, stringify the message before replaying it.
+  let messageToReplay = message
+
+  if (message.constructor.name === 'String') {
+    try {
+      JSON.parse(message)
+    } catch (err) {
+      messageToReplay = JSON.stringify(message)
+    }
+  }
+  context.bindings.processFewsEventCode = messageToReplay
 }
