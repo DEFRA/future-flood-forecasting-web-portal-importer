4c6046b9a1bec088d500307868f4f480335ae4ff
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2021-01-08 15:51:30 +0000

    Improve clarity and use of default values

--- Shared/connection-pool.js
+++ Shared/connection-pool.js
@@ -1,17 +1,18 @@
 const sql = require('mssql')
 const { getEnvironmentVariableAsAbsoluteInteger, isBoolean } = require('./utils')
 const { logger } = require('defra-logging-facade')
+const hostJson = require('../host.json')
 
 module.exports = function () {
   // Configure database connectivity using:
   // - some mssql defaults
   // - some custom defaults
   // - selected environment variable based customisation.
-  const abortTransactionOnError = isBoolean(process.env.SQLDB_ABORT_TRANSACTION_ON_ERROR) ? process.env.SQLDB_ABORT_TRANSACTION_ON_ERROR : false
   const port = getEnvironmentVariableAsAbsoluteInteger('SQLDB_PORT')
-  const connectionTimeout = getEnvironmentVariableAsAbsoluteInteger('SQLDB_CONNECTION_TIMEOUT')
-  const requestTimeout = getEnvironmentVariableAsAbsoluteInteger('SQLDB_REQUEST_TIMEOUT')
+  const connectionTimeout = getEnvironmentVariableAsAbsoluteInteger('SQLDB_CONNECTION_TIMEOUT_MILLIS')
+  const requestTimeout = getEnvironmentVariableAsAbsoluteInteger('SQLDB_REQUEST_TIMEOUT_MILLIS')
   const maxRetriesOnTransientErrors = getEnvironmentVariableAsAbsoluteInteger('SQLDB_MAX_RETRIES_ON_TRANSIENT_ERRORS')
+  const packetSize = getEnvironmentVariableAsAbsoluteInteger('SQLDB_PACKET_SIZE')
   const maxPooledConnections = getEnvironmentVariableAsAbsoluteInteger('SQLDB_MAX_POOLED_CONNECTIONS')
   const minPooledConnections = getEnvironmentVariableAsAbsoluteInteger('SQLDB_MIN_POOLED_CONNECTIONS')
   const acquireTimeoutMillis = getEnvironmentVariableAsAbsoluteInteger('SQLDB_ACQUIRE_TIMEOUT_MILLIS')
@@ -21,26 +22,62 @@ module.exports = function () {
   const reapIntervalMillis = getEnvironmentVariableAsAbsoluteInteger('SQLDB_REAP_INTERVAL_MILLIS')
   const createRetryIntervalMillis = getEnvironmentVariableAsAbsoluteInteger('SQLDB_CREATE_RETRY_INTERVAL_MILLIS')
 
+  const maxConcurrentCalls = hostJson.extensions.serviceBus.messageHandlerOptions.maxConcurrentCalls
+
   const config = {
     user: process.env.SQLDB_USER,
     password: process.env.SQLDB_PASSWORD,
     server: process.env.SQLDB_SERVER,
-    port: port || 1433,
     database: process.env.SQLDB_DATABASE,
-    connectionTimeout: connectionTimeout || 15000,
-    requestTimeout: requestTimeout || 15000,
-    maxRetriesOnTransientErrors: maxRetriesOnTransientErrors || 3,
-    abortTransactionOnError: abortTransactionOnError,
+    requestTimeout: requestTimeout || 60000,
     pool: {
-      max: maxPooledConnections || 10,
-      min: minPooledConnections || 6,
-      acquireTimeoutMillis: acquireTimeoutMillis || 30000,
-      createTimeoutMillis: createTimeoutMillis || 30000,
-      destroyTimeoutMillis: destroyTimeoutMillis || 5000,
-      idleTimeoutMillis: idleTimeoutMillis || 30000,
-      reapIntervalMillis: reapIntervalMillis || 1000,
-      createRetryIntervalMillis: createRetryIntervalMillis || 200
+      min: minPooledConnections || maxConcurrentCalls + 1,
+      max: maxPooledConnections || maxConcurrentCalls * 2
     }
+  }
+
+  if (port) {
+    config.port = port
+  }
+
+  if (connectionTimeout) {
+    config.connectionTimeout = connectionTimeout
+  }
+
+  if (maxRetriesOnTransientErrors) {
+    config.maxRetriesOnTransientErrors = maxRetriesOnTransientErrors
+  }
+
+  if (packetSize) {
+    config.packetSize = packetSize
+  }
+
+  if (isBoolean(process.env.SQLDB_ABORT_TRANSACTION_ON_ERROR)) {
+    config.abortTransactionOnError = JSON.parse(process.env.SQLDB_ABORT_TRANSACTION_ON_ERROR)
+  }
+
+  if (acquireTimeoutMillis) {
+    config.pool.acquireTimeoutMillis = acquireTimeoutMillis
+  }
+
+  if (createTimeoutMillis) {
+    config.pool.createTimeoutMillis = createTimeoutMillis
+  }
+
+  if (destroyTimeoutMillis) {
+    config.pool.destroyTimeoutMillis = destroyTimeoutMillis
+  }
+
+  if (idleTimeoutMillis) {
+    config.pool.idleTimeoutMillis = idleTimeoutMillis
+  }
+
+  if (reapIntervalMillis) {
+    config.pool.reapIntervalMillis = reapIntervalMillis
+  }
+
+  if (createRetryIntervalMillis) {
+    config.pool.createRetryIntervalMillis = createRetryIntervalMillis
   }
 
   this.pool = new sql.ConnectionPool(config)
