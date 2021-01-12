049e1fadc5b8a8e78f8f1e7baf1b0a3d2df723e2
Adam-W1 <adam.wallace@environment-agency.gov.uk>
2021-01-12 12:41:47 +0000

    test coverage increase

--- Shared/shared-refresh-csv-rows.js
+++ Shared/shared-refresh-csv-rows.js
@@ -122,8 +122,8 @@ async function buildPreparedStatementParameters (context, row, refreshData) {
 
 async function processCsvRow (context, preparedStatement, row, refreshData) {
   try {
-    const result = await buildPreparedStatementParameters(context, row, refreshData)
-    if (result.rowError) {
+    const rowExecuteObject = await buildPreparedStatementParameters(context, row, refreshData)
+    if (rowExecuteObject.rowError) {
       context.log.warn('row is missing data.')
       const failedRowInfo = {
         rowData: row,
@@ -132,7 +132,7 @@ async function processCsvRow (context, preparedStatement, row, refreshData) {
       }
       refreshData.failedCsvRows.push(failedRowInfo)
     } else {
-      await preparedStatement.execute(result)
+      await preparedStatement.execute(rowExecuteObject)
     }
   } catch (err) {
     context.log.warn(`An error has been found in a row.\nError : ${err}.`)
