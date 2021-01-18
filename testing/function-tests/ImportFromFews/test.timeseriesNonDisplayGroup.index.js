b73d79cdeff64538d03947607abe49884c828232
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2021-01-18 14:41:27 +0000

    Use JSON.parse

--- testing/function-tests/ImportFromFews/test.timeseriesNonDisplayGroup.index.js
+++ testing/function-tests/ImportFromFews/test.timeseriesNonDisplayGroup.index.js
@@ -3,6 +3,7 @@ const importFromFewsMessages = require('./messages/non-display-group-messages')
 const ImportFromFewsTestUtils = require('./import-from-fews-test-utils')
 const ConnectionPool = require('../../../Shared/connection-pool')
 const Context = require('../mocks/defaultContext')
+const { isBoolean } = require('../../../Shared/utils')
 const timeseriesTypeConstants = require('../../../ImportFromFews/helpers/timeseries-type-constants')
 const moment = require('moment')
 const sql = require('mssql')
@@ -650,7 +651,8 @@ module.exports = describe('Tests for import timeseries non-display groups', () =
     }
 
     // The following check is for when there is an output binding named 'stagedTimeseries' active
-    if (!!process.env.IMPORT_TIMESERIES_OUTPUT_BINDING_REQUIRED === true) {
+    if (isBoolean(process.env.IMPORT_TIMESERIES_OUTPUT_BINDING_REQUIRED) &&
+        JSON.parse(process.env.IMPORT_TIMESERIES_OUTPUT_BINDING_REQUIRED)) {
       for (const stagedTimeseries of context.bindings.stagedTimeseries) {
         expect(receivedPrimaryKeys).toContainEqual(stagedTimeseries.id)
       }
