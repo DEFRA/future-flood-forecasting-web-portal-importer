6cb68448fa408c8fc1648ae4b88b65fa4eeb8b8e
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2021-03-11 15:19:53 +0000

    Fix/log replayed messages correctly part two (#186)
    * Fix/log replayed messages correctly part two
    
    Part of https://eaflood.atlassian.net/browse/IWP-638
    
    * Correct message logging
    
    * Update version number

--- ReplayProcessFewsEventCode/index.js
+++ ReplayProcessFewsEventCode/index.js
@@ -1,5 +1,4 @@
 module.exports = async function (context, message) {
-  context.log(`Replaying ${message}`)
   // If JSON parsing fails when a message is sent to the fews-eventcode queue the message
   // will remain on the dead letter queue. It seems that some core forecasting engine messages
   // can contain invalid JSON, causing this scanario. To workaround this temporarily, try and parse
@@ -13,5 +12,12 @@ module.exports = async function (context, message) {
       messageToReplay = JSON.stringify(message)
     }
   }
+
+  if (message.constructor.name === 'Object') {
+    context.log(`Replaying ${JSON.stringify(messageToReplay)}`)
+  } else {
+    context.log(`Replaying ${messageToReplay}`)
+  }
+
   context.bindings.processFewsEventCode = messageToReplay
 }
