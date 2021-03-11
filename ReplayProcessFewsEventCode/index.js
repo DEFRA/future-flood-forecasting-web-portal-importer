c8485e3597b6d4e83384e0755f91dc5598eb2374
Adam Wallace <Adam.wallace@environment-agency.gov.uk>
2021-03-11 16:54:58 +0000

    Feature/custom service bus expiration (#182)
    Initiate service bus queue custom message expiration
    
    https://eaflood.atlassian.net/browse/IWP-734

--- ReplayProcessFewsEventCode/index.js
+++ ReplayProcessFewsEventCode/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, message) {
   // If JSON parsing fails when a message is sent to the fews-eventcode queue the message
   // will remain on the dead letter queue. It seems that some core forecasting engine messages
-  // can contain invalid JSON, causing this scanario. To workaround this temporarily, try and parse
+  // can contain invalid JSON, causing this scenario. To workaround this temporarily, try and parse
   // the message as JSON before replay is attempted. If parsing fails, stringify the message before replaying it.
   let messageToReplay = message
 
