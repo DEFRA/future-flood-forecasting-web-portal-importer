04f92526e76b9e444607d0bc4ef30e728fd2300c
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2021-05-19 14:04:53 +0100

    Refactor for maintainability

--- ImportFromFews/helpers/process-import-error.js
+++ ImportFromFews/helpers/process-import-error.js
@@ -3,38 +3,37 @@ const getPiServerErrorMessage = require('../../Shared/timeseries-functions/get-p
 const TimeseriesStagingError = require('../../Shared/timeseries-functions/timeseries-staging-error')
 
 module.exports = async function (context, taskRunData, err) {
-  let errorData
   if (!(err instanceof TimeseriesStagingError) && typeof err.response === 'undefined') {
     context.log.error(`Failed to connect to ${process.env.FEWS_PI_API}`)
     // If connection to the PI Server fails propagate the failure so that standard Azure message replay
     // functionality is used.
     throw err
   } else {
-    // For other errors create a timeseries staging exception to indicate that
-    // manual intervention is required before replay of the task run is attempted.
-    if (err instanceof TimeseriesStagingError) {
-      errorData = err.context
-    } else {
-      errorData = await createTimeseriesStagingExceptionData(context, taskRunData, err)
-    }
+    const errorData = await prepareTimeseriesStagingExceptionData(context, taskRunData, err)
     await createTimeseriesStagingException(context, errorData)
   }
 }
 
-async function createTimeseriesStagingExceptionData (context, taskRunData, err) {
-  const csvError = (err.response && err.response.status === 400) || false
-  const csvType = csvError ? taskRunData.csvType : null
-  const piServerErrorMessage = await getPiServerErrorMessage(context, err)
-  const errorDescription = `An error occurred while processing data for ${taskRunData.sourceTypeDescription} ${taskRunData.sourceId} of task run ${taskRunData.taskRunId} (workflow ${taskRunData.workflowId}): ${piServerErrorMessage}`
-  return {
-    transaction: taskRunData.transaction,
-    sourceId: taskRunData.sourceId,
-    sourceType: taskRunData.sourceType,
-    csvError: csvError,
-    csvType: csvType,
-    fewsParameters: taskRunData.fewsParameters || null,
-    payload: taskRunData.message,
-    timeseriesHeaderId: taskRunData.timeseriesHeaderId,
-    description: errorDescription
+async function prepareTimeseriesStagingExceptionData (context, taskRunData, err) {
+  let errorData
+  if (err instanceof TimeseriesStagingError) {
+    errorData = err.context
+  } else {
+    const csvError = (err.response && err.response.status === 400) || false
+    const csvType = csvError ? taskRunData.csvType : null
+    const piServerErrorMessage = await getPiServerErrorMessage(context, err)
+    const errorDescription = `An error occurred while processing data for ${taskRunData.sourceTypeDescription} ${taskRunData.sourceId} of task run ${taskRunData.taskRunId} (workflow ${taskRunData.workflowId}): ${piServerErrorMessage}`
+    errorData = {
+      transaction: taskRunData.transaction,
+      sourceId: taskRunData.sourceId,
+      sourceType: taskRunData.sourceType,
+      csvError: csvError,
+      csvType: csvType,
+      fewsParameters: taskRunData.fewsParameters || null,
+      payload: taskRunData.message,
+      timeseriesHeaderId: taskRunData.timeseriesHeaderId,
+      description: errorDescription
+    }
   }
+  return errorData
 }
