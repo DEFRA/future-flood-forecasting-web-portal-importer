c939ff34e51a8971af40ca5ebe77046cb2fce088
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2021-05-19 16:31:45 +0100

    Refactor for maintainability

---
+++ ImportFromFews/helpers/process-task-run-data-for-non-forecast-or-latest-task-run-for-workflow-if-possible.js
@@ -0,0 +1,16 @@
+const isLatestTaskRunForWorkflow = require('../../Shared/timeseries-functions/is-latest-task-run-for-workflow')
+const isNonDisplayGroupForecast = require('./is-non-display-group-forecast')
+
+module.exports = async function (context, taskRunData, checkForNonDisplayGroupForecast, processingFunction) {
+  if (await isNonForecastOrLatestTaskRunForWorkflow(context, taskRunData, checkForNonDisplayGroupForecast)) {
+    await processingFunction(context, taskRunData)
+  } else {
+    context.log.warn(`Ignoring message for ${taskRunData.sourceTypeDescription} ${taskRunData.sourceId} of task run ${taskRunData.taskRunId} (workflow ${taskRunData.workflowId}) completed on ${taskRunData.taskRunCompletionTime}` +
+    ` - ${taskRunData.latestTaskRunId} completed on ${taskRunData.latestTaskRunCompletionTime} is the latest task run for workflow ${taskRunData.workflowId}`)
+  }
+}
+
+async function isNonForecastOrLatestTaskRunForWorkflow (context, taskRunData, checkForNonDisplayGroupForecast) {
+  const isForecast = checkForNonDisplayGroupForecast ? isNonDisplayGroupForecast(context, taskRunData) : taskRunData.forecast
+  return !isForecast || await isLatestTaskRunForWorkflow(context, taskRunData)
+}
