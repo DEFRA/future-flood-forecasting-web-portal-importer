a41a1dc107b62ad6e75949f72edf19144e934587
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2021-05-20 16:24:01 +0100

    Refactor for maintainability

--- Shared/timeseries-functions/deactivate-staging-exceptions-by-source-function-and-task-run-id.js
+++ Shared/timeseries-functions/deactivate-staging-exceptions-by-source-function-and-task-run-id.js
@@ -1,6 +1,34 @@
 const sql = require('mssql')
 const { executePreparedStatementInTransaction } = require('../transaction-helper')
 
+const query = `
+  insert into
+    fff_staging.inactive_staging_exception (staging_exception_id)
+  select
+    se.id
+  from
+    fff_staging.staging_exception se
+  where
+  task_run_id = @taskRunId and
+  coalesce(
+    source_function,
+    convert(nvarchar(1), case
+      when payload like '%description%' then 'P'
+      when payload like '%taskRunId%' then 'I'
+      end
+    )
+  ) = @sourceFunction and
+  not exists
+    (
+      select
+        1
+      from
+        fff_staging.inactive_staging_exception ise
+      where
+        ise.staging_exception_id = se.id
+    )
+`
+
 module.exports = async function (context, stagingExceptionData) {
   const transaction = stagingExceptionData.transaction
   await executePreparedStatementInTransaction(deactivateStagingException, context, transaction, stagingExceptionData)
@@ -10,33 +38,7 @@ async function deactivateStagingException (context, preparedStatement, stagingEx
   await preparedStatement.input('sourceFunction', sql.NVarChar)
   await preparedStatement.input('taskRunId', sql.NVarChar)
 
-  await preparedStatement.prepare(`
-    insert into
-      fff_staging.inactive_staging_exception (staging_exception_id)
-    select
-      se.id
-    from
-      fff_staging.staging_exception se
-    where
-      task_run_id = @taskRunId and
-      coalesce(
-        source_function,
-        convert(nvarchar(1), case
-          when payload like '%description%' then 'P'
-          when payload like '%taskRunId%' then 'I'
-          end
-        )
-      ) = @sourceFunction and
-      not exists
-        (
-          select
-            1
-          from
-            fff_staging.inactive_staging_exception ise
-          where
-            ise.staging_exception_id = se.id
-        )
-  `)
+  await preparedStatement.prepare(query)
 
   const parameters = {
     taskRunId: stagingExceptionData.taskRunId,
