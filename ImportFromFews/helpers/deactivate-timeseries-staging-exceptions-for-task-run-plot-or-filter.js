7854747b314e71ce822b3fcaa3704e7afeeda2c2
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2021-05-24 17:56:59 +0100

    Refactor for maintainability

--- ImportFromFews/helpers/deactivate-timeseries-staging-exceptions-for-task-run-plot-or-filter.js
+++ ImportFromFews/helpers/deactivate-timeseries-staging-exceptions-for-task-run-plot-or-filter.js
@@ -1,6 +1,30 @@
 const { executePreparedStatementInTransaction } = require('../../Shared/transaction-helper')
 const sql = require('mssql')
 
+const query = `
+  insert into
+    fff_staging.inactive_timeseries_staging_exception (timeseries_staging_exception_id)
+  select
+    tse.id
+  from
+    fff_staging.timeseries_staging_exception tse
+    inner join fff_staging.timeseries_header th
+      on tse.timeseries_header_id = th.id  
+  where
+    th.id = tse.timeseries_header_id and
+    th.task_run_id = @taskRunId and
+    tse.source_id = @sourceId and
+    tse.source_type = @sourceType and
+    not exists
+      (
+        select
+          1
+        from
+          fff_staging.inactive_timeseries_staging_exception itse
+        where
+          itse.timeseries_staging_exception_id = tse.id
+      )
+`
 module.exports = async function (context, taskRunData) {
   await executePreparedStatementInTransaction(deactivateTimeseriesStagingExceptionsForTaskRunPlotOrFilter, context, taskRunData.transaction, taskRunData)
 }
@@ -10,30 +34,7 @@ async function deactivateTimeseriesStagingExceptionsForTaskRunPlotOrFilter (cont
   await preparedStatement.input('sourceId', sql.NVarChar)
   await preparedStatement.input('sourceType', sql.NVarChar)
 
-  await preparedStatement.prepare(`
-    insert into
-      fff_staging.inactive_timeseries_staging_exception (timeseries_staging_exception_id)
-    select
-      tse.id
-    from
-      fff_staging.timeseries_staging_exception tse
-      inner join fff_staging.timeseries_header th
-        on tse.timeseries_header_id = th.id  
-    where
-      th.id = tse.timeseries_header_id and
-      th.task_run_id = @taskRunId and
-      tse.source_id = @sourceId and
-      tse.source_type = @sourceType and
-      not exists
-        (
-          select
-            1
-          from
-            fff_staging.inactive_timeseries_staging_exception itse
-          where
-            itse.timeseries_staging_exception_id = tse.id
-        )
-  `)
+  await preparedStatement.prepare(query)
 
   const parameters = {
     taskRunId: taskRunData.taskRunId,
