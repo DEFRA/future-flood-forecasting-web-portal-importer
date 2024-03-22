d076528436aabca59972e195e72e68790c798d3b
takanocap <107683587+takanocap@users.noreply.github.com>
2024-03-22 11:50:55 +0000

    add the river name to the fluvial forecast location data (#386)
    * add the river name to the fluvial forecast location data
    
    * refactor unit test for river name
    
    * Correct column name mix up
    
    * fix for column name mix up
    
    * Revert "fix for column name mix up"
    
    This reverts commit e77c6b5be03ec9138864ea4ad00d90a617a49772.
    
    * Revert "Correct column name mix up"
    
    This reverts commit 953b2c2b0b1cb794943e9cd708cb28ce6b80d642.
    
    * Revert "refactor unit test for river name"
    
    This reverts commit 977dc3f14e3e32a6f51dc8311372b5f108029156.
    
    * Correct column definition
    
    * Add river name support
    
    * Use corresponding database scripting branch temporarily
    
    * Update version number
    
    * Revert "Use corresponding database scripting branch temporarily"
    
    This reverts commit 9935a7327ce3a5f168a18b8b199a22cd45c4c45c.
    
    * Resolve linting issues
    
    ---------
    
    Co-authored-by: pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>

--- RefreshFluvialForecastLocationData/index.js
+++ RefreshFluvialForecastLocationData/index.js
@@ -4,6 +4,7 @@ const commonRefreshData = require('../Shared/csv-load/common-refresh-data')
 const functionSpecificData = [
   { tableColumnName: 'FFFS_LOCATION_ID', tableColumnType: 'NVarChar', expectedCSVKey: 'FFFSLocID' },
   { tableColumnName: 'FFFS_LOCATION_NAME', tableColumnType: 'NVarChar', expectedCSVKey: 'FFFSLocName' },
+  { tableColumnName: 'RIVER_LOCAL', tableColumnType: 'NVarChar', expectedCSVKey: 'RiverLocal', nullValueOverride: true },
   { tableColumnName: 'DRN_ORDER', tableColumnType: 'Int', expectedCSVKey: 'DRNOrder' },
   { tableColumnName: 'DATUM', tableColumnType: 'NVarChar', expectedCSVKey: 'Datum', nullValueOverride: true },
   { tableColumnName: 'DISPLAY_ORDER', tableColumnType: 'Int', expectedCSVKey: 'Order' },
@@ -16,7 +17,6 @@ const functionSpecificData = [
   { tableColumnName: 'LOCATION_Y', tableColumnType: 'Decimal', expectedCSVKey: 'LocationY', precision: 38, scale: 8, nullValueOverride: true, preprocessor: commonRefreshData.returnNullForNaN },
   { tableColumnName: 'LOCATION_Z', tableColumnType: 'Decimal', expectedCSVKey: 'LocationZ', precision: 38, scale: 8, nullValueOverride: true, preprocessor: commonRefreshData.returnNullForNaN }
 ]
-
 module.exports = async function (context) {
   const refreshData = {
     csvUrl: process.env.FLUVIAL_FORECAST_LOCATION_URL,
@@ -27,9 +27,9 @@ module.exports = async function (context) {
     countStatement: 'select count(*) as number from fff_staging.fluvial_forecast_location',
     insertPreparedStatement: `
       insert into 
-        fff_staging.fluvial_forecast_location (fffs_location_id, fffs_location_name, drn_order, datum, display_order, centre, plot_id, catchment, catchment_order, mfdo_area, location_x, location_y, location_z)       
+        fff_staging.fluvial_forecast_location (fffs_location_id, fffs_location_name, river_local, drn_order, datum, display_order, centre, plot_id, catchment, catchment_order, mfdo_area, location_x, location_y, location_z)
       values 
-        (@fffs_location_id, @fffs_location_name, @drn_order, @datum, @display_order, @centre, @plot_id, @catchment, @catchment_order, @mfdo_area, @location_x, @location_y, @location_z)`,
+        (@fffs_location_id, @fffs_location_name, @river_local, @drn_order, @datum, @display_order, @centre, @plot_id, @catchment, @catchment_order, @mfdo_area, @location_x, @location_y, @location_z)`,
     // Column information and corresponding csv information
     functionSpecificData
   }
