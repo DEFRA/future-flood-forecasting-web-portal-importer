5aadf5d2a270897e51d41fb35e2aa8fee4285ee2
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-04-22 17:02:31 +0100

    Increase test coverage

--- testing/function-tests/shared/test.transaction-helper.js
+++ testing/function-tests/shared/test.transaction-helper.js
@@ -1,10 +1,11 @@
 const sql = require('mssql')
-const { doInTransaction } = require('../../../Shared/transaction-helper')
+const { doInTransaction, executePreparedStatementInTransaction } = require('../../../Shared/transaction-helper')
 const ConnectionPool = require('../../../Shared/connection-pool')
 const Context = require('../mocks/defaultContext')
 
 const jestConnectionPool = new ConnectionPool()
 const pool = jestConnectionPool.pool
+const errorMessage = 'ERROR'
 
 let context
 
@@ -23,28 +24,131 @@ module.exports = describe('Test transaction helper', () => {
       // Closing the DB connection allows Jest to exit successfully.
       await pool.close()
     })
-    it('should throw an exception following a failure to commit a transaction', async () => {
-      const errorMessage = 'ERROR'
-      jest.spyOn(sql, 'Request').mockImplementation(() => {
-        return {
-          batch: jest.fn(),
-          query: jest.fn()
-        }
-      })
-      jest.spyOn(sql, 'Transaction').mockImplementation(() => {
-        return {
-          begin: jest.fn(),
-          commit: jest.fn().mockImplementation(() => {
-            throw new Error(errorMessage)
+    it('should throw an exception following a failure to commit a transaction', done => {
+      jest.isolateModules(async () => {
+        try {
+          jest.spyOn(sql, 'Request').mockImplementation(() => {
+            return {
+              batch: jest.fn(),
+              query: jest.fn()
+            }
           })
+          jest.spyOn(sql, 'Transaction').mockImplementation(() => {
+            return {
+              begin: jest.fn(),
+              commit: jest.fn().mockImplementation(() => {
+                throw new Error(errorMessage)
+              })
+            }
+          })
+          await expect(doInTransaction({ fn: () => {}, context, errorMessage: 'Error' })).rejects.toThrow(errorMessage)
+          done()
+        } catch (err) {
+          done(err)
         }
       })
-      await expect(doInTransaction({ fn: performQuery, context, errorMessage: 'Error' })).rejects.toThrow(errorMessage)
+    })
+    it('should throw an exception following a failure to unprepare a PreparedStatement', done => {
+      jest.isolateModules(async () => {
+        try {
+          jest.spyOn(sql, 'PreparedStatement').mockImplementation(() => {
+            return {
+              prepare: jest.fn(),
+              prepared: true,
+              unprepare: jest.fn().mockImplementation(() => {
+                throw new Error(errorMessage)
+              })
+            }
+          })
+          jest.spyOn(sql, 'Request').mockImplementation(() => {
+            return {
+              batch: jest.fn(),
+              query: jest.fn()
+            }
+          })
+          jest.spyOn(sql, 'Transaction').mockImplementation(() => {
+            return {
+              begin: jest.fn(),
+              commit: jest.fn()
+            }
+          })
+          await expect(doInTransaction({ fn: testExecutePreparedStatementInTransaction, context, errorMessage: 'Error' })).rejects.toThrow(errorMessage)
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
+    })
+    it('should throw an exception if a transaction is not present following a failure', done => {
+      jest.isolateModules(async () => {
+        try {
+          jest.spyOn(sql, 'Request').mockImplementation(() => {
+            return {
+              batch: jest.fn(),
+              query: jest.fn()
+            }
+          })
+          jest.spyOn(sql, 'Transaction').mockImplementation(() => {})
+          await expect(doInTransaction({ fn: () => {}, context, errorMessage: 'Error' })).rejects.toThrow('transaction.begin is not a function')
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
+    })
+    it('should not attempt to commit an aborted transaction', done => {
+      jest.isolateModules(async () => {
+        try {
+          jest.spyOn(sql, 'Request').mockImplementation(() => {
+            return {
+              batch: jest.fn(),
+              query: jest.fn()
+            }
+          })
+          jest.spyOn(sql, 'Transaction').mockImplementation(() => {
+            return {
+              _aborted: true,
+              begin: jest.fn(),
+              commit: jest.fn().mockImplementation(() => {
+                throw new Error('COMMIT ERROR')
+              })
+            }
+          })
+          await expect(doInTransaction({ fn: () => { throw new Error(errorMessage) }, context, errorMessage: 'Error' })).rejects.toThrow(errorMessage)
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
+    })
+    it('should not attempt to commit a transaction for which rollback has been requested', done => {
+      jest.isolateModules(async () => {
+        try {
+          jest.spyOn(sql, 'Request').mockImplementation(() => {
+            return {
+              batch: jest.fn(),
+              query: jest.fn()
+            }
+          })
+          jest.spyOn(sql, 'Transaction').mockImplementation(() => {
+            return {
+              _rollbackRequested: true,
+              begin: jest.fn(),
+              commit: jest.fn().mockImplementation(() => {
+                throw new Error('COMMIT ERROR')
+              })
+            }
+          })
+          await expect(doInTransaction({ fn: () => { throw new Error(errorMessage) }, context, errorMessage: 'Error' })).rejects.toThrow(errorMessage)
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
     })
   })
 })
 
-async function performQuery (transaction, context) {
-  const request = sql.Request(transaction)
-  await request.query('select 1')
+async function testExecutePreparedStatementInTransaction (transaction, context) {
+  await executePreparedStatementInTransaction(() => {}, context, transaction)
 }
