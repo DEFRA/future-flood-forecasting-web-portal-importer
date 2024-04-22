f03e24f6ddf02c4f947afb866acdb76e9414459a
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-04-22 12:45:12 +0100

    Simplify unit tests

--- testing/function-tests/shared/test.transaction-helper.js
+++ testing/function-tests/shared/test.transaction-helper.js
@@ -1,29 +1,27 @@
 const sql = require('mssql')
 const { doInTransaction } = require('../../../Shared/transaction-helper')
-const CommonTimeseriesTestUtils = require('./common-timeseries-test-utils')
 const ConnectionPool = require('../../../Shared/connection-pool')
 const Context = require('../mocks/defaultContext')
 
 const jestConnectionPool = new ConnectionPool()
 const pool = jestConnectionPool.pool
-const commonTimeseriesTestUtils = new CommonTimeseriesTestUtils(pool)
 
 let context
 
 module.exports = describe('Test transaction helper', () => {
   describe('The transaction helper', () => {
     beforeAll(async () => {
-      await commonTimeseriesTestUtils.beforeAll(pool)
+      await pool.connect()
     })
     beforeEach(async () => {
       // As mocks are reset and restored between each test (through configuration in package.json), the Jest mock
       // function implementation for the function context needs creating for each test.
       context = new Context()
-      await commonTimeseriesTestUtils.beforeEach(pool)
     })
 
     afterAll(async () => {
-      await commonTimeseriesTestUtils.afterAll(pool)
+      // Closing the DB connection allows Jest to exit successfully.
+      await pool.close()
     })
     it('should throw an exception following a failure to commit a transaction', async () => {
       const errorMessage = 'ERROR'
