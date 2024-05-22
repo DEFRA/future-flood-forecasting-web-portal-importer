bd81da7e25aaf2498f9c024c963451c964f0035a
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-05-22 11:49:29 +0100

    Refactor for mocking and remove catch block

--- Shared/service-bus-helper.js
+++ Shared/service-bus-helper.js
@@ -1,17 +1,15 @@
 // Code adapted from https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-nodejs-how-to-use-queues-new-package
-const { ServiceBusClient } = require('@azure/service-bus')
+const azureServiceBus = require('@azure/service-bus')
 const connectionString = process.env.AzureWebJobsServiceBus
 
 module.exports = {
   publishMessages: async function (config, ...args) {
-    const sbClient = new ServiceBusClient(connectionString)
+    const sbClient = new azureServiceBus.ServiceBusClient(connectionString)
     const sender = sbClient.createSender(config.destinationName)
     try {
       const messages = await config.fn(config.context, ...args)
       await sender.sendMessages(messages)
       await sender.close()
-    } catch (err) {
-      console.log('Error occurred: ', err)
     } finally {
       await sbClient.close()
     }
