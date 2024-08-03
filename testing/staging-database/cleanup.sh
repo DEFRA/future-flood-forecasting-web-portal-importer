8b000fdfe93dbb2d2390d21672d2b8e8d4f10661
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-08-03 15:20:59 +0100

    Replace docker-compose

--- testing/staging-database/cleanup.sh
+++ testing/staging-database/cleanup.sh
@@ -2,7 +2,7 @@
 
 if  [[ -d testing/staging-database/future-flood-forecasting-web-portal-staging ]]; then
   cd testing/staging-database/future-flood-forecasting-web-portal-staging
-  docker-compose down --rmi all
+  docker compose down --rmi all
   cd ..
   rm -rf future-flood-forecasting-web-portal-staging
   echo "******** Unit test staging database has been destroyed "
